15c9db15a49e1aebaa786d875c0d573a26655d09
Yuwen <56615246+Ahram-41@users.noreply.github.com>
2024-04-07 17:04:22 +0800

    chore: reorganize content generator dir and update config file

---
+++ ppt_generator/config.py
@@ -0,0 +1,32 @@
+"""
+This module contains configuration settings for the DSA4213 PPT Generator.
+
+API_KEY: str
+    The API key used for authentication.
+
+REMOTE_ADDRESS: str
+    The remote address of the server used for communication.
+"""
+
+from pydantic_settings import BaseSettings
+
+API_KEY = ""
+REMOTE_ADDRESS = "https://h2ogpte.genai.h2o.ai"
+PDF_PATH = "./data/attention.pdf"
+PPT_DIR = "./dsa4213-ppt-generator/my_ppts/"
+PPT_MODE_DIR = "./ppt_generator/ppt_mode/"
+
+
+# class Settings(BaseSettings):
+#     APP_NAME: str = "AppName"
+#     SQLALCHEMY_URL: str
+#     ENVIRONMENT: str
+
+#     class Config:
+#         env_file = ".env"  # Works with uvicorn run command from my-app/project/
+#         # env_file = "../.env"  Works with alembic command from my-app/alembic
+#         # env_file = abs_path_env
+
+
+# def get_settings():
+#     return Settings()
